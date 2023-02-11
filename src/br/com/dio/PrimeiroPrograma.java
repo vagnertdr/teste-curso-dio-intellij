58eb8ceadab452cba851835c61b39000c69f6a75
Vagner Rocha <vagnerrochab2@gmail.com>
2023-02-11 05:23:26 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+        /*
+        int a = 5;
+        int b = 3;
+        System.out.println("Hello world! " + (a + b));*/
+        // para apagar a linha CTRL Y
+        // Para refactorar um programa SHIFT + F6
+        // CTRL + D duplica uma linha
+    }
+}
